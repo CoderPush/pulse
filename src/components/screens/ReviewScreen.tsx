8ed26399da308b3b4c08f1a855ff8e422ed36081
Minhchau0108 <74655110+Minhchau0108@users.noreply.github.com>
2025-04-25 11:38:54 +0700

    fix: fetch request (#6)
    * fix: fetch request
    
    * fix: add cors

--- src/components/screens/ReviewScreen.tsx
+++ src/components/screens/ReviewScreen.tsx
@@ -18,6 +18,7 @@ export default function ReviewScreen({ onBack, formData, onNext }: ScreenProps) 
         headers: {
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
         body: JSON.stringify(formData),
       });
 
