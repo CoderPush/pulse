89c88451db890a205db848868cefb4e0bbc7d5ec
Minhchau0108 <74655110+Minhchau0108@users.noreply.github.com>
2025-04-29 13:28:57 +0700

    fix: remove unused docs (#25)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,8 @@
-import type { NextRequest } from 'next/server'
+import { type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
-
 export async function middleware(request: NextRequest) {
-  // Handle session update
-  const response = await updateSession(request)
-
-  // Get the origin from the request headers
-  const origin = request.headers.get('origin') || '*'
-
-  // Add CORS headers to the response
-  response.headers.set('Access-Control-Allow-Credentials', 'true')
-  response.headers.set('Access-Control-Allow-Origin', origin)
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  response.headers.set(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
-  )
-
-  return response
+  return await updateSession(request)
 }
-
-// Configure which routes to run middleware on
 export const config = {
   matcher: [
     /*
